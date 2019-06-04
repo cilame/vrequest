debfd44555d4a84a237459b57f1c199e7bb80af9
cilame <opaquism@hotmail.com>
2019-06-04 12:36:53 +0800

    add

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
  
 setup(
     name = "vrequest",
-    version = "1.4.8",
+    version = "1.4.9",
     keywords = "vrequest",
     author = "cilame",
     author_email = "opaquism@hotmail.com",
