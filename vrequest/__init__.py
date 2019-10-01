93ea2901645a933f01d7a18ea6f7be7b468485be
cilame <opaquism@hotmail.com>
2019-10-02 01:44:03 +0800

    无依赖的xpath功能还是有点弱，后续再增强吧。

--- vrequest/__init__.py
+++ vrequest/__init__.py
@@ -1,4 +1,4 @@
 __author__ = 'cilame'
-__version__ = '1.7.22'
+__version__ = '1.7.23'
 __email__ = 'opaquism@hotmail.com'
 __github__ = 'https://github.com/cilame/vrequest'
--- vrequest/util.py
+++ vrequest/util.py
@@ -844,13 +844,13 @@ class Vnode:
         for i, (k, m, n) in enumerate(p, 1):
             r[i] = []; ps = _pc(m, n); depth = float('inf') if k.startswith('//') else 1
             attrs = ps if type(ps) == dict else {}; one = ps if type(ps) == int  else None
-            if (i != len(p) or i == 1) and type(ps) != list:
-                for s in ms: r[i].extend(self.find_by_maps(s, m, attrs=attrs, depth=depth, one=one))
-                ms = r[i]; r[i - 1] = None; continue
-            for s in ms:
-                if   ps and ps[0] =='attrs': s = s['info']['attrs'].get(ps[1])
-                elif ps and ps[0] =='data':  s = s['info']['data']
-                if s: r[i].append(s)
+            if (i != len(p) or i == 1): [r[i].extend(self.find_by_maps(s, m, attrs=attrs, depth=depth, one=one)) for s in ms]
+            elif type(ps) != list:      [r[i].extend(self.find_by_maps(s, m, attrs=attrs, depth=depth, one=one)) for s in ms]
+            else:
+                for s in ms:
+                    if   ps and ps[0] =='attrs': s = s['info']['attrs'].get(ps[1])
+                    elif ps and ps[0] =='data':  s = s['info']['data']
+                    if s: r[i].append(s)
             ms = r[i]; r[i - 1] = None
         return [Vnode(i) if type(i) == dict else i for i in ms]
 class VHTML:
